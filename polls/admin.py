8da345b90ad514d019cd6842fc53f0f729db4ccd
eymenisleksoy <7541323@gmal.com>
2026-03-22 20:19:59 +0300

    Hide Question and Choice from Admin Recent Actions

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,22 @@
 from django.contrib import admin
 from .models import Question, Choice, Comment
 
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Comment)
+class NoLogAdmin(admin.ModelAdmin):
+    def log_addition(self, request, object, message):
+        pass
+    def log_change(self, request, object, message):
+        pass
+    def log_deletion(self, request, object, object_repr):
+        pass
+
+@admin.register(Question)
+class QuestionAdmin(NoLogAdmin):
+    pass
+
+@admin.register(Choice)
+class ChoiceAdmin(NoLogAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
